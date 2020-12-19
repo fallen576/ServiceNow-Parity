74f7469cb66d9c2c8955febe95b72527a4cd1a99
Ben Gianni <bgianni576@gmail.com>
2020-12-19 12:19:35 -0500

    started on create table view

--- src/main/webapp/angular/app/snp.createtablecontroller.js
+++ src/main/webapp/angular/app/snp.createtablecontroller.js
@@ -4,23 +4,50 @@
     angular.module('ServiceNowParity')
         .controller('CreateTableController', CreateTableController);
 
+    CreateTableController.$inject = ['$http'];
+    function CreateTableController($http) {
 
-    function CreateTableController() {
-        
         var c = this;
-        c.fields = [];
-        
-        c.addField = function() {
-            c.fields.push(1);
+        c.fields = [{
+            "fieldName": "",
+            "fieldType": ""
+        }];
+        c.name = "";
+        c.error = false;
+
+        c.addField = function () {
+
+            if (c.fields.length === 0) {
+                c.fields.push({
+                    "fieldName": "",
+                    "fieldType": ""
+                });
+                return;
+            }
+
+            if (c.fields[c.fields.length-1].fieldName == "" || c.fields[c.fields.length-1].fieldType == "") {
+                c.error = true;
+                return;
+            }
+
+            c.error = false;
+
+            c.fields.push({
+                "fieldName": "",
+                "fieldType": ""
+            });
         };
 
-        c.removeField = function() {
+        c.removeField = function () {
             c.fields.pop();
         }
-
-        c.submit = function() {
-            
+    
+        c.submit = function () {
+            $http({
+                url: ''
+            });
         };
-    }
 
-})();
+    };
+
+}) ();
