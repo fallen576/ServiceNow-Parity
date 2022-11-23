ce5bdcb00023b78285aa6fd7467a1b1dca8d9fb4
Ben Gianni <bgianni576@gmail.com>
2022-11-23 16:02:19 -0500

    added elasticsearch test

--- src/main/java/com/snp/data/es/model/ESLog.java
+++ src/main/java/com/snp/data/es/model/ESLog.java
@@ -36,6 +36,14 @@ public class ESLog {
         this.source = source;
         this.user = user;
     }
+    
+    public String getId() {
+    	return id;
+    }
+    
+    public void setId(String id) {
+    	this.id = id;
+    }
 
 	public String getText() {
 		return text;
